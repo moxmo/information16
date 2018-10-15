b4490ef7cedbfa89451fb3c8cc7c4060d35c2e4b
xwp <xwp_fullstack@163.com>
2018-10-15 13:55:49 +0800

    数据库配置

--- manager.py
+++ manager.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 
 class Config(object):
+
     DEBUG = True
+    SECRET_KEY = "HI"
+
 
     SQLALCHEMY_DATABASE_URI = "mysql://route:mysql@localhost:information16"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -19,7 +23,9 @@ db = SQLAlchemy(app)
 
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 
-@app.route("/")
+#CSRFProtect(app)
+
+@app.route("/",methods=["POST"])
 def helloworld():
 
     redis_store.set("name","li")
