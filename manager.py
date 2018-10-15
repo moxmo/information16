d4baca4dd272c9bb8d9b331e59d90d65fe6eafa4
xwp <xwp_fullstack@163.com>
2018-10-15 14:23:22 +0800

    数据库配置session

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf import CSRFProtect
@@ -17,19 +18,30 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 3600*24*2#两天期限
+
+
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 
-#CSRFProtect(app)
+CSRFProtect(app)
 
-@app.route("/",methods=["POST"])
+Session(app)
+
+@app.route("/")
 def helloworld():
 
     redis_store.set("name","li")
     print(redis_store.get("name"))
+
+    session ["age"] = "13"
+    print(session.get("age"))
 
     return "helloworld100"
 
