924f9fbb10691ccc9a2117240e1de595694966fd
xwp <xwp_fullstack@163.com>
2018-10-15 11:29:58 +0800

    数据库配置

--- manager.py
+++ manager.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql://route:mysql@localhost:information16"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def helloworld():
     return "helloworld100"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
