6f45853bdae33444682c77ca3a4d8f914ec71de0
xwp <xwp_fullstack@163.com>
2018-10-16 14:39:31 +0800

    网站logo 显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,6 @@
 from info import redis_store
 from . import index_blu
-from flask import render_template
+from flask import render_template,current_app
 
 @index_blu.route("/")
 def helloworld():
@@ -25,3 +25,12 @@ def helloworld():
     # current_app.logger.error("错误信息2")
 
     return render_template("news/index.html")
+
+
+#处理网站logo
+#每个浏览器在访问服务器的时候,会自动向该服务器发送一个get 请求,地址是:/favicon.ico
+#获取静态文件夹资源:current_app.send_static_file("文件路径“),会自动寻找static静态文件夹的资源
+@index_blu.route('/favicon.ico')
+def web_logo():
+
+    return current_app.send_static_file("news/favicon.ico")
