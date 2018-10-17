fca7f42d2b0c8da5053e30fcf69d45634cb1bece
xwp <xwp_fullstack@163.com>
2018-10-17 09:09:32 +0800

    短信验证码后端实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,11 +1,79 @@
-from flask import current_app
+import random
+import re
+from flask import current_app, jsonify
+from flask import json
 from flask import make_response
 from flask import request
 
 from info import constants
 from info import redis_store
+from info.libs.yuntongxun.sms import CCP
+from info.utils.response_code import RET
 from . import passport_blue
 from info.utils.captcha.captcha import captcha
+
+#获取短信验证码
+#请求地址:/passport/sms_code
+#请求方式:POST
+#请求参数:mobile,image_code,image_code_id
+#返回值：errno,errmsg
+@passport_blue.route('/sms_code', methods=['POST'])
+def sms_code():
+    """
+    1.获取参数
+    2.校验参数,为空检验
+    3.校验手机号格式
+    4.根据传入的图片验证码编号获取redis中的图片验证码A
+    5.判断图片验证码A,是否过期
+    6.删除redis中的图片验证码A
+    7.判断传入的验证码B, 和redis中的验证码A是否相等
+    8.生成短信验证码
+    9.调用CCP发送,并判断
+    10.将短信验证码保存一份到redis
+    11.返回响应
+    :return:
+    """
+    #1.获取参数
+    json_data = request.data
+    dict_data = json.loads(json_data)
+    mobile = dict_data.get("mobile")
+    image_code = dict_data.get("image_code")
+    image_code_id = dict_data.get("image_code_id")
+    #2.校验参数, 为空检验
+    if not all([mobile,image_code,image_code_id]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数不能为空")
+    #3.校验手机号格式
+    if not re.match("1[356789]\d{9}",mobile):
+        return jsonify(errno=RET.DATAERR,errmsg="手机号格式不正确")
+    #4.根据传入的图片验证码编号获取redis中的图片验证码A
+    redis_image_code = ("image_code:%s"%image_code_id)
+
+    #5.判断图片验证码A, 是否过期
+    if not redis_image_code:
+        return jsonify(errno=RET.NODATA,errmsg="图片验证码过期")
+
+    #6.删除redis中的图片验证码A
+    redis_store.delete("image_code:%s"%image_code_id)
+
+    #7.判断传入的验证码B, 和redis中的验证码A是否相等
+    if image_code_id != redis_image_code:
+        return jsonify(errno=RET.DATAERR,errmsg="图片验证码填写错误")
+
+    #8.生成短信验证码
+    sms_code = "%06d"%random.randint(0,999999)
+
+    #9.调用CCP发送, 并判断
+    ccp = CCP()
+    result = ccp.send_template_sms(mobile,[sms_code,5],1)
+
+    if result == -1:
+        return jsonify(errno=RET.DATAERR,errmsg="短信发送失败")
+
+    #10.将短信验证码保存一份到redis
+    redis_store.set("sms_code:%s"%mobile,sms_code,constants.SMS_CODE_REDIS_EXPIRES)
+    #11.返回响应
+    return jsonify(errno=RET.OK,errmsg="短信发送成功")
+
 
 #获取图片验证码
 #请求方式:/passport/image_code
