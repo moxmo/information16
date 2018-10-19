49ce5f1f882c47818ecec4e28a9ae557501970f5
xwp <xwp_fullstack@163.com>
2018-10-19 14:11:30 +0800

    新闻详情页面数据获取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,3 +1,8 @@
+from flask import abort
+from flask import current_app, jsonify
+
+from info.models import News
+from info.utils.response_code import RET
 from . import news_blue
 from flask import render_template
 
@@ -9,4 +14,18 @@ from flask import render_template
 @news_blue.route('/<int:news_id>')
 def news_detail(news_id):
 
-    return render_template("news/detail.html")
+
+    #根据新闻编号获取新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="新闻获取失败")
+    #判断新闻对象是否存在,后续会对404做统一处理
+    if not news:
+        abort(404)
+    #携带新闻数据,到模板页面展示
+    data = {
+        "news":news.to_dict()
+    }
+    return render_template("news/detail.html",data=data)
