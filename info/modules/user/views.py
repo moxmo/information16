ab40914f2f003d6147ceb166457e225811941250
xwp <xwp_fullstack@163.com>
2018-10-21 21:25:56 +0800

    个人用户密码修改

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -7,6 +7,42 @@ from info.utils.response_code import RET
 from . import user_blue
 from flask import render_template
 
+# - 密码修改
+# 请求路径: /user/pass_info
+# 请求方式: GET,POST
+# 请求参数: GET无  POST请求有参数,old_password,new_password
+# 返回值: user_pass_info.html页面,data字典数据,POST请求：errno,errmsg
+@user_blue.route('/pass_info', methods=['GET', 'POST'])
+@user_login_data
+def pass_info():
+    pass
+
+#   - 1.判断请求方式,如果是GET,渲染页面
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html")
+#   - 2.如果是POST请求,获取参数
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+#   - 3.为空校验参数
+    if not all ([old_password,new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+#   - 4.判断旧密码是否正确
+    if not g.user.check_passowrd(old_password):
+        return jsonify(errno=RET.DATAERR, errmsg="旧密码错误")
+
+#   - 5.修改新密码
+    g.user.password = new_password
+
+#   - 6.返回响应
+    return jsonify(errno=RET.OK, errmsg="修改成功")
+
+
+
+
+
+
 #功能：展示基本信息资料
 # 请求路径: /user/base_info
 # 请求方式: GET,POST
